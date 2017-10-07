818e59327c487e6908d48a8eea1d22db21c6d959
tayatoshi <tsk0414z@gmail.com>
2017-10-07 11:09:48 +0900

    change cd

--- fish/config.fish
+++ fish/config.fish
@@ -6,10 +6,6 @@ source ~/.config/fish/alias.fish
 # iterm2 shell integration
 source ~/.iterm2_shell_integration.fish
 
-## editorをnvimに設定
-set -x EDITOR nvim
-set -x SVN_EDITOR nvim
-
 ## 環境変数
 #
 # SSHで接続した先で日本語が使えるようにする
@@ -24,12 +20,40 @@ set -x LANG ja_JP.UTF-8
 #nvimの設定ファイル
 set XDG_CONFIG_HOME '~/.config'
 
-## cdしたあとで、自動的に ls -Gする
+## cdしたあとで、自動的に ls -aする
 function cd
-    builtin cd $argv
-	ls -a
-end
+    if test (count $argv) -gt 1
+        printf "%s\n" (_ "Too many args for cd command")
+        return 1
+    end
+    # Avoid set completions.
+    set -l previous $PWD
 
+    if test "$argv" = "-"
+        if test "$__fish_cd_direction" = "next"
+            nextd
+        else
+            prevd
+        end
+        return $status
+    end
+    builtin cd $argv
+    set -l cd_status $status
+    # Log history
+    if test $cd_status -eq 0 -a "$PWD" != "$previous"
+        set -q dirprev[$MAX_DIR_HIST]
+        and set -e dirprev[1]
+        set -g dirprev $dirprev $previous
+        set -e dirnext
+        set -g __fish_cd_direction prev
+    end
+
+    if test $cd_status -ne 0
+        return 1
+    end
+    ls
+    return $status
+end
 ## pyenvのpath指定
 status --is-interactive; and . (pyenv init -| psub)
 
