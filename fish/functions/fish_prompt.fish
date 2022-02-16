cdf29077686ddf73e38f314159ee4d6bee8061dc
tayatoshi <tsk14sns@gmail.com>
2022-02-16 17:56:36 +0900

    fishのプロンプトの変更

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,92 +1,17 @@
-function fish_prompt
-	set -l __last_command_exit_status $status
-
-    if not set -q -g __fish_robbyrussell_functions_defined
-        set -g __fish_robbyrussell_functions_defined
-        function _git_branch_name
-            set -l branch (git symbolic-ref --quiet HEAD ^/dev/null)
-            if set -q branch[1]
-                echo (string replace -r '^refs/heads/' '' $branch)
-            else
-                echo (git rev-parse --short HEAD ^/dev/null)
-            end
-        end
-
-        function _is_git_dirty
-            echo (git status -s --ignore-submodules=dirty ^/dev/null)
-        end
-
-        function _is_git_repo
-            type -q git
-            or return 1
-            git status -s >/dev/null ^/dev/null
-        end
-
-        function _hg_branch_name
-            echo (hg branch ^/dev/null)
-        end
-
-        function _is_hg_dirty
-            echo (hg status -mard ^/dev/null)
-        end
-
-        function _is_hg_repo
-            type -q hg
-            or return 1
-            hg summary >/dev/null ^/dev/null
-        end
-
-        function _repo_branch_name
-            eval "_$argv[1]_branch_name"
-        end
-
-        function _is_repo_dirty
-            eval "_is_$argv[1]_dirty"
-        end
-
-        function _repo_type
-            if _is_hg_repo
-                echo 'hg'
-            else if _is_git_repo
-                echo 'git'
-            end
-        end
+function fish_prompt --description 'Write out the prompt'
+   if test -n "$SSH_TTY"
+        echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
 
-    set -l cyan (set_color -o cyan)
-    set -l yellow (set_color -o yellow)
-    set -l red (set_color -o red)
-    set -l green (set_color -o green)
-    set -l blue (set_color -o blue)
-    set -l normal (set_color normal)
+    # PWD
+    echo -n (set_color --bold blue)(prompt_pwd)
+    set_color normal
+    echo
 
-    set -l arrow_color "$green"
-    if test $__last_command_exit_status != 0
-        # set arrow_color "$red"
-        set arrow_color "$green"
+    if fish_is_root_user
+        echo -n (set_color red)'# '
     end
-
-    set -l arrow "$arrow_color➜ "
-    set -l arrow2 "$red❯$yellow❯$green❯"
-    if [ $USER = 'root' ]
-        set arrow "$arrow_color# "
-    end
-
-	set -l name $green $USER
-
-    set -l repo_type (_repo_type)
-    if [ $repo_type ]
-        # set -l repo_branch $red(_repo_branch_name $repo_type)
-        set -l repo_branch $cyan(_repo_branch_name $repo_type)
-        # set repo_info "$blue $repo_type:($repo_branch$blue)"
-        set repo_info "$cyan [$repo_branch$cyan]"
-
-        if [ (_is_repo_dirty $repo_type) ]
-            set -l dirty "$yellow✗"
-            set repo_info "$repo_info$dirty"
-        end
-    end
-
-	# echo -n -s $name $arrow ' '$cwd $repo_info $normal ' '
-	echo -n -s $name $repo_info ' ' $arrow2 $normal ' '
+    echo -n (set_color --bold green)(echo $USER)  (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
+    set_color normal
+    
 end
