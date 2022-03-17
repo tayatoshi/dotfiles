baa1005ff24e31bdd3735b56f1f3b9975f644790
tayatoshi <tsk14sns@gmail.com>
2022-03-17 14:36:22 +0900

    ファイルの配置を変更に伴いsymlink用のshell scriptを修正

--- shellscript/install_dotfiles.sh
+++ shellscript/install_dotfiles.sh
@@ -1,24 +1,22 @@
 
 set -euxo pipefail
 
-# test='~/Desktop/test'
-# echo $test
 DIRCONFIG=~/.config
 
 if [ ! -d $DIRCONFIG ]; then
   echo 'mkdir $DIRCONFIG'
   mkdir $DIRCONFIG
 fi
-#
+
 if [ ! -d $DIRCONFIG/nvim ]; then
-  ln -sf ~/dotfiles/nvim ~/.config/nvim
+  ln -sf ~/dotfiles/.config/nvim ~/.config/nvim
 fi
 
 if [ ! -d $DIRCONFIG/fish ]; then
-  ln -sf ~/dotfiles/fish ~/.config/fish
+  ln -sf ~/dotfiles/.config/fish ~/.config/fish
 fi
 
-ln -sf ~/dotfiles/starship/starship.toml ~/.config/starship.toml
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/R/.Rprofile ~/.Rprofile
+ln -sf ~/dotfiles/.config/starship.toml ~/.config/starship.toml
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.Rprofile ~/.Rprofile
 echo '---SymLink Done.---'
