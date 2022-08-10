6eace31dce031ffebf341936d483a4d125ce333a
tayatoshi <16584891+tayatoshi@users.noreply.github.com>
2022-08-10 14:47:19 +0900

    [neovim] pythonのパスをanyenvに対応させるために修正

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -30,7 +30,8 @@ vim.o.signcolumn = "yes"
 opt.swapfile = false
 
 opt.statusline = "%{%v:lua.require'nvim-navic'.get_location()%}"
-vim.g.python3_host_prog='$HOME/.pyenv/versions/neovim3/bin/python3'
+vim.g.python3_host_prog='$HOME/.anyenv/envs/pyenv/versions/3.10.6/bin/python3'
 vim.g.doge_doc_standard_python = 'google'
 
 vim.g['fern#default_hidden'] = 1
+
