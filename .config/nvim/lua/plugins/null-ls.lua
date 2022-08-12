4c6912df04d399afc403ed5edba29d20e0ba63ed
tayatoshi <16584891+tayatoshi@users.noreply.github.com>
2022-08-12 14:49:50 +0900

    [neovim] 不要なコメントを削除

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -1,17 +1,11 @@
 require("null-ls").setup({
     sources = {
         -- Formatting
-		-- require("null-ls").builtins.formatting.stylua,      --Formatter for lua
         require("null-ls").builtins.formatting.black,        --Formatter for python
-        -- require("null-ls").builtins.formatting.prettier,        --Formatter for html, css, js, ts and others
         require("null-ls").builtins.formatting.sql_formatter,       --Formatter for sql
 
         -- Diagnostics
-        -- require("null-ls").builtins.diagnostics.eslint,     --Diagnostics for js, ts, and others
         require("null-ls").builtins.diagnostics.flake8,     --Diagnostics for python
         require("null-ls").builtins.diagnostics.sqlfluff,     --Diagnostics for sql
-        -- require("null-ls").builtins.diagnostics.stylelint,     --Diagnostics for css
-        -- require("null-ls").builtins.diagnostics.tidy,     --Diagnostics for html
-        -- require("null-ls").builtins.diagnostics.luacheck,     --Diagnostics for lua
     },
 })
