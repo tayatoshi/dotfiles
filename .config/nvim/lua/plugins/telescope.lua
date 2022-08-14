3150dfa0266e8cad5f77d7022b75b2ae9f221e1b
tayatoshi <16584891+tayatoshi@users.noreply.github.com>
2022-08-14 14:26:02 +0900

    [neovim] キーマップはkeymaps.luaに移動

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,11 +1,3 @@
--- mapping to open telescope in vim
-vim.api.nvim_set_keymap("n", "<leader>ff", [[<Cmd>lua require('telescope.builtin').find_files({hidden=true})<CR>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", [[<Cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", [[<Cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", [[<Cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fm", [[<Cmd>lua require('telescope.builtin').keymaps()<CR>]], { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>gb", [[<Cmd>lua require('telescope.builtin').git_branches()<CR>]], { noremap = true })
-
 local telescope = require('telescope')
 local actions = require('telescope.actions')
 
